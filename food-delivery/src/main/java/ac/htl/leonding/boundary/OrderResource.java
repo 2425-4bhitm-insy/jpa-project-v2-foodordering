f0603b27ac7b15b5cf0e27a8c1f4b646fc53c4c3
michael <m.stuetzner@students.htl-leonding.ac.at>
2025-03-25 09:15:22 +0100

    added: basic imports, Paths and temporary methods in Resources

--- food-delivery/src/main/java/ac/htl/leonding/boundary/OrderResource.java
+++ food-delivery/src/main/java/ac/htl/leonding/boundary/OrderResource.java
@@ -1,4 +1,14 @@
 package ac.htl.leonding.boundary;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+
+@Path("/orders")
 public class OrderResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTemp() {
+        return "This is a temporary method!";
+    }
 }
--- food-delivery/src/main/java/ac/htl/leonding/boundary/RestaurantResource.java
+++ food-delivery/src/main/java/ac/htl/leonding/boundary/RestaurantResource.java
@@ -1,4 +1,14 @@
 package ac.htl.leonding.boundary;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+
+@Path("/restaurants")
 public class RestaurantResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTemp() {
+        return "This is a temporary method!";
+    }
 }
