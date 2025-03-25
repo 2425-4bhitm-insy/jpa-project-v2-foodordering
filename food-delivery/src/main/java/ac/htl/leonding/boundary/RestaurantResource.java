25a467fb001d19b449f5701bf4cd58a12abf76b4
michael <m.stuetzner@students.htl-leonding.ac.at>
2025-03-25 10:12:53 +0100

    added: Inject and Requests in RestaurantResource (commented out)

--- food-delivery/src/main/java/ac/htl/leonding/boundary/RestaurantResource.java
+++ food-delivery/src/main/java/ac/htl/leonding/boundary/RestaurantResource.java
@@ -1,14 +1,35 @@
 package ac.htl.leonding.boundary;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 
 @Path("/restaurants")
 public class RestaurantResource {
 
+//    @Inject
+//    RestaurantRepository restaurantRepository;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getTemp() {
         return "This is a temporary method!";
     }
+
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response addRestaurant(Restaurant restaurant) {
+//        return Response.ok(
+//                restaurantRepository.addRestaurant(restaurant))
+//                .build();
+//    }
+//
+//    @GET
+//    @Path("/{id}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response findRestaurantById(@PathParam("id") Long id) {
+//        return Response.ok(
+//                restaurantRepository.findRestaurantById(id))
+//                .build();
+//    }
 }
