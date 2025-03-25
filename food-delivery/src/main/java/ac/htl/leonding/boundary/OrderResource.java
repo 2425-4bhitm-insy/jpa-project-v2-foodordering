36355da142492004951970c0a5e21549f4d2d4c0
michael <m.stuetzner@students.htl-leonding.ac.at>
2025-03-25 10:11:09 +0100

    added: Inject and Requests in OrderResource (commented out)

--- food-delivery/src/main/java/ac/htl/leonding/boundary/OrderResource.java
+++ food-delivery/src/main/java/ac/htl/leonding/boundary/OrderResource.java
@@ -1,14 +1,51 @@
 package ac.htl.leonding.boundary;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 
 @Path("/orders")
 public class OrderResource {
 
+//    @Inject
+//    OrderRepository orderRepository;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getTemp() {
         return "This is a temporary method!";
     }
+
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response addOrder(Order order) {
+//        return Response.ok(
+//                orderRepository.addOrder(order))
+//                .build();
+//    }
+//
+//    @GET
+//    @Path("/{id}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response findOrderById(@PathParam("id") Long id) {
+//        return Response.ok(
+//                orderRepository.findOrderById(id))
+//                .build();
+//    }
+//
+//    @PUT
+//    @Path("/{id}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response updateOrder(@PathParam("id") Long id, Order order) {
+//        return Response.ok(
+//                orderRepository.updateOrder(id, order))
+//                .build();
+//    }
+//
+//    @DELETE
+//    @Path("/{id}")
+//    public Response deleteOrder(@PathParam("id") Long id) {
+//        orderRepository.deleteOrder(id);
+//        return Response.noContent().build();
+//    }
 }
