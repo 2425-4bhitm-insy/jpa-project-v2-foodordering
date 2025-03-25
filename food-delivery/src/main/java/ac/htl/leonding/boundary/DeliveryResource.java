03c543a02892828a468ea191892d3eaa054bb859
michael <m.stuetzner@students.htl-leonding.ac.at>
2025-03-25 10:15:21 +0100

    added: Inject and Requests in DeliveryResource (commented out)

--- food-delivery/src/main/java/ac/htl/leonding/boundary/DeliveryResource.java
+++ food-delivery/src/main/java/ac/htl/leonding/boundary/DeliveryResource.java
@@ -1,14 +1,27 @@
 package ac.htl.leonding.boundary;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 
 @Path("/deliveries")
 public class DeliveryResource {
 
+//    @Inject
+//    DeliveryRepository deliveryRepository;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getTemp() {
         return "This is a temporary method!";
     }
+
+//    @POST
+//    @Path("/{id}/status")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response updateDeliveryStatus(@PathParam("id") Long id, DeliveryStatus status) {
+//        return Response.ok(
+//                deliveryRepository.findDeliveryById(id).updateDeliveryStatus(status))
+//                .build();
+//    }
 }
